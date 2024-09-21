c6004cd7c24fb1b88ba9d7833cf8044f7793fca7
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-09-21 15:34:48 +0200

    Integrated hugo react frontend with express netlify functions backend for items service

--- static/react/App.tsx
+++ static/react/App.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import Header from "./components/blocks/Header/Header";
 import ThumbnailContainer from "./components/features/CoffeeShop/ThumbnailContainer/ThumbnailContainer";
 
 function App() {
-  const items = [
+  /*const items = [
     { itemId: "1", imageId: "/images/200x200.png", title: "Drip Coffee", price: 0, description: "A product", salePrice: 0 },
     { itemId: "2", imageId: "/images/200x200.png", title: "Cookie", price: 0, description: "A product", salePrice: 0 },
     { itemId: "3", imageId: "/images/200x200.png", title: "Croissant", price: 0, description: "A product", salePrice: 0 },
     { itemId: "4", imageId: "/images/200x200.png", title: "Cupcake", price: 0, description: "A product", salePrice: 0 },
     { itemId: "5", imageId: "/images/200x200.png", title: "French Press", price: 0, description: "A product", salePrice: 0},
     { itemId: "6", imageId: "/images/200x200.png", title: "Iced Coffee", price: 0, description: "A product", salePrice: 0},
-  ];
+  ];*/
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    axios.get("/api/items")
+    .then((result) => setItems(result.data))
+    .catch(console.error)
+  }, [])
+
   return (
     <div className="pt-5 mt-5">
       <Header />
