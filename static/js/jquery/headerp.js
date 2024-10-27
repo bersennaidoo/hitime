e78650c7523486f64294064f9314f4d075995526
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-10-27 12:23:27 +0200

    Refactored media queries for sm md and lg devices for nav hero and home page

--- static/js/jquery/headerp.js
+++ static/js/jquery/headerp.js
@@ -1,17 +1,17 @@
-$(function() {
-    var $dropdownToggle = $(".c-headerp__dropdown-toggle")
-    let $dropdownMenu = $(".c-headerp__dropdown-menu")
-    var isOpen = false
+// Hide and Show dropdown on mobile devices
+$(function () {
+  var $dropdownToggle = $(".c-headerp-grid-item__dropdown-toggle");
+  let $dropdownMenu = $(".c-headerp-grid-item__dropdown-menu");
+  var isOpen = false;
 
-    $($dropdownToggle).on("click", function(e) {
-        e.preventDefault()
-        if (isOpen === false) {
-        $($dropdownMenu).css("display", "block")
-        isOpen = true
-        } else {
-            $($dropdownMenu).css({"display": "none"})
-            $(".c-headerp").css({"margin": 0, "padding": 0})
-            isOpen = false
-        }
-    })
-})
+  $($dropdownToggle).on("click", function (e) {
+    e.preventDefault();
+    if (isOpen === false) {
+      $($dropdownMenu).css("display", "block");
+      isOpen = true;
+    } else {
+      $($dropdownMenu).css({ display: "none" });
+      isOpen = false;
+    }
+  });
+});
